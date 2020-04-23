ea27eac0d1a84cda4abd4e137c9079bbb784d1c3
Quyen Le <heyquyenle@gmail.com>
2020-04-22 18:05:41 -0700

    updating

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,12 +4,12 @@ var path = require('path');
 module.exports = function(app){
 //Get route to display the survey page
  app.get('/survey', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../public/survey.html'));
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
   });
 
  //USE route to display the home page
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, '/../public/home.html'));
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
   });
 
 };
