4f8bd05ed72e5e2578a53edc2b1002411ad8448b
Quyen Le <heyquyenle@gmail.com>
2020-04-22 17:50:04 -0700

    deploying heroku app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendMatch = [
     {
       name: "Jennifer Lawrence",
-      photo: "https://media.vanityfair.com/photos/5d02ab2776711f03ed7fe6a3/master/pass/jennifer-lawrence-relationship.jpg"
+      photo: "https://media.vanityfair.com/photos/5d02ab2776711f03ed7fe6a3/master/pass/jennifer-lawrence-relationship.jpg",
       scores: [
         "4",
         "5",
@@ -12,11 +12,8 @@ var friendMatch = [
         "4",
         "4",
         "3",
-        "4",
-    ]
-    }
-  
-  ];
+        "4"
+    ]}];
   
   
   
